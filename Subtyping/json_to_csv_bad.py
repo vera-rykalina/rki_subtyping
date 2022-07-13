f96b65f5a0e4ca6b7be4cca20d046d95493038b1
vera-rykalina <verarykalina@gmail.com>
2022-07-13 13:25:35 +0200

    Add some comments, cleanup

--- Subtyping/json_to_csv_bad.py
+++ Subtyping/json_to_csv_bad.py
@@ -1,21 +1,8 @@
 #!/bin/python
 
 """
-import pandas as pd
-# As of Pandas 1.01, json_normalize as pandas.io.json.json_normalize is deprecated and is now exposed in the top-level namespace.
-# from pandas.io.json import json_normalize
-from pathlib import Path
-import json
-
-# set path to file
-p = Path(r'c:\some_path_to_file\test.json')
-
-# read json
-with p.open('r', encoding='utf-8') as f:
-    data = json.loads(f.read())
-
-# create dataframe
-df = pd.json_normalize(data)
+This is a script to convert .json to .csv.
+This code though does not provide a desired result (a clean .csv file).
 """
 
 import json
