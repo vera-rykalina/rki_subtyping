314ac72fc0b05425675baf4362e9ea7b5301e530
vera-rykalina <verarykalina@gmail.com>
2022-07-13 14:47:29 +0200

    Add df.to_csv() chunk and comments,  modify splitting

--- Subtyping/json_to_csv_good.py
+++ Subtyping/json_to_csv_good.py
@@ -1,33 +1,44 @@
 #!/bin/python3
 
+# Import libraries
 import json
 import pandas as pd
 
-
-
+# Open JSON file
 with open("baker.json") as f:
     data = json.load(f)
 
+
+# Initiate lists and dictionary
 columns = {}
 col_header = []
 col_subtype = []
-for index, sequence in enumerate(data):
-    #print(sequence['subtypeText'])
-    #print(index, sequence['subtypeText'])
+
+# Extract useful info
+for sequence in data:
     col_header.append(sequence["inputSequence"]["header"])
     col_subtype.append(sequence["subtypeText"])
     
-
+# Add info to the dictionary
 columns["SequenceName"] = col_header
 columns["Subtype%"] = col_subtype
+
+# Create a dataframe
 df = pd.DataFrame(columns)
 
+# Split subtype and % components of the string
+# Create a new columns "Comment"
+# str.rstrip - splits string around given separator, starting from the right
+df[["Subtype", "Comment"]] = df["Subtype%"].str.rsplit(" ", n=1, expand=True)
 
-print(len(data))
-print(type(data))
-print(type(data[0]))
-#print(df.head())
 
-df[["Subtype", "Percent"]] = df["Subtype%"].str.split(" ", n=1, expand=True)
+# Remove original subtype column <A (5.08%)>
+df = df.drop(columns=["Subtype%"])
+
+# Check output
 print(df.head())
 print(df.tail())
+
+
+# Convert a plsandas dataframe to .csv file
+df.to_csv("stanford_PRRT.csv",index=False, sep=",", encoding='utf-8')
