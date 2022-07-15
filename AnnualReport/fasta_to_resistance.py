2a443cb0a3938ca25fb06d505b295eb935d8ad44
vera-rykalina <verarykalina@gmail.com>
2022-07-15 07:23:31 +0200

    Add condition in the loop for index 57

--- AnnualReport/fasta_to_resistance.py
+++ AnnualReport/fasta_to_resistance.py
@@ -21,18 +21,27 @@ col_ATV_interpretation = []
 for index, sequence in enumerate(data):
    col_header.append(sequence["inputSequence"]["header"])
    col_strain.append(sequence["strain"]["name"])
-   if len(sequence["drugResistance"]) == 4:
+   if index == 57 and sequence["drugResistance"][1]["drugScores"][0] != "PI":
+    col_gene.append(sequence["drugResistance"][1]["gene"]["name"])
+    col_class.append("NA")
+    col_ATV_score.append("NA")
+    col_ATV_level.append("NA")
+    col_ATV_interpretation.append("NA")
+    
+   #if len(sequence["drugResistance"]) == 4:
+   else:
     col_gene.append(sequence["drugResistance"][2]["gene"]["name"])
     col_class.append(sequence["drugResistance"][2]["drugScores"][0]["drugClass"]["name"])
     col_ATV_score.append(sequence["drugResistance"][2]["drugScores"][0]["score"])
     col_ATV_level.append(sequence["drugResistance"][2]["drugScores"][0]["level"])
     col_ATV_interpretation.append(sequence["drugResistance"][2]["drugScores"][0]["text"])
-   else:
-    col_gene.append("NA")
-    col_class.append("NA")
-    col_ATV_score.append("NA")
-    col_ATV_level.append("NA")
-    col_ATV_interpretation.append("NA")
+#    else:
+#     print(index)
+#     col_gene.append("NA")
+#     col_class.append("NA")
+#     col_ATV_score.append("NA")
+#     col_ATV_level.append("NA")
+#     col_ATV_interpretation.append("NA")
 
   
 columns["SequenceName"] = col_header
@@ -45,17 +54,6 @@ columns["ATV/r Interpretation"] = col_ATV_interpretation
 df = pd.DataFrame(columns)
 
 df.to_csv("resistance.csv", index=False, sep=",", encoding='utf-8')
-
-# Some quick check points
-# print(len(data[0]["drugResistance"]))
-# print(type(data[0]['drugResistance'][2]['drugScores']))
-# print(len(data[0]['drugResistance'][2]['drugScores']))
-# print("=============================")
-# print(type(data[0]["drugResistance"][2]['drugScores'][0]['drugClass']['name']))
-# print(data[0]["drugResistance"][2]['drugScores'][0]['drugClass']['name'])
-# print(type(data))
-# print(type(data[0]))
-
 
 
 print(df.head())
