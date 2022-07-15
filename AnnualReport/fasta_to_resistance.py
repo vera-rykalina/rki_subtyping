cd686b69725a83800ae328ba11cd2382699e5589
vera-rykalina <verarykalina@gmail.com>
2022-07-15 12:52:54 +0200

    Delete commented blocks and modify if statement

--- AnnualReport/fasta_to_resistance.py
+++ AnnualReport/fasta_to_resistance.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 
 import json
-from matplotlib.cbook import ls_mapper
 import pandas as pd
 
 
@@ -21,27 +20,19 @@ col_ATV_interpretation = []
 for index, sequence in enumerate(data):
    col_header.append(sequence["inputSequence"]["header"])
    col_strain.append(sequence["strain"]["name"])
-   if index == 57 and sequence["drugResistance"][1]["drugScores"][0] != "PI":
-    col_gene.append(sequence["drugResistance"][1]["gene"]["name"])
+   if index == 57 and len(sequence["drugResistance"]) < 4:
+    col_gene.append("NA")
     col_class.append("NA")
     col_ATV_score.append("NA")
     col_ATV_level.append("NA")
     col_ATV_interpretation.append("NA")
     
-   #if len(sequence["drugResistance"]) == 4:
    else:
     col_gene.append(sequence["drugResistance"][2]["gene"]["name"])
     col_class.append(sequence["drugResistance"][2]["drugScores"][0]["drugClass"]["name"])
     col_ATV_score.append(sequence["drugResistance"][2]["drugScores"][0]["score"])
     col_ATV_level.append(sequence["drugResistance"][2]["drugScores"][0]["level"])
     col_ATV_interpretation.append(sequence["drugResistance"][2]["drugScores"][0]["text"])
-#    else:
-#     print(index)
-#     col_gene.append("NA")
-#     col_class.append("NA")
-#     col_ATV_score.append("NA")
-#     col_ATV_level.append("NA")
-#     col_ATV_interpretation.append("NA")
 
   
 columns["SequenceName"] = col_header
