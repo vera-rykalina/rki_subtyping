d6083e625cf012771b0e9b3d0aa8e7e4592cdc1b
vera-rykalina <verarykalina@gmail.com>
2022-07-19 14:12:29 +0200

    Add a loop over rows to exclude NA subtypes

--- Subtyping/stanford_parser.py
+++ Subtyping/stanford_parser.py
@@ -2,9 +2,11 @@
 
 # Import libraries
 import json
+import numpy as np
 import pandas as pd
 
 path = "/Users/vera/Learning/CQ/Internship/rki_subtyping_resistance/Subtyping/stanford/MS95_PRRT_20.json"
+
 
 # Open JSON file
 with open(path) as f:
@@ -28,18 +30,34 @@ columns["Subtype%"] = col_subtype
 # Create a dataframe
 df = pd.DataFrame(columns)
 
-# Split subtype and % components of the string
-# Create a new columns "Comment"
-# str.rstrip - splits a string by a separator, starting from the right
-df[["Subtype", "Comment"]] = df["Subtype%"].str.rsplit(" ", n=1, expand=True)
+
+# Iterate over row in df (some sequence may have NAs as subtype)
+# Create new columns "Subtype" and "Comment"
+
+for i, row in df.iterrows():
+    if row["Subtype%"] == "NA":
+        df.at[i, ["Subtype", "Comment"]] = "NA"
+    
+    else:
+        # split subtype and % components in the string
+        # str.rstrip - splits a string by a separator, starting from the right
+        df[["Subtype", "Comment"]] = df["Subtype%"].str.rsplit(" ", n=1, expand=True)
 
 
-# Remove original subtype column as formatted: A (5.08%)
-df = df.drop(columns=["Subtype%"])
+df["Comment"] = df["Comment"].fillna("NA")
+
+
+# Check that type of "Comments" is a string now
+print(type(df.loc[0,"Comment"]))
+
+
+# Remove original subtype column as formatted: A (5.08%), it works in place
+#df.drop(columns=["Subtype%"], inplace = True)
+df.drop("Subtype%", axis=1, inplace = True)
 
 # Check output
 print(df.head())
-print(df.tail())
+#print(df.tail())
 
 
 # Convert a pandas dataframe to a .csv file
