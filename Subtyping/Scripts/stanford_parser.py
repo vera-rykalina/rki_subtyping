90b12c6ae5f7438bdbafa19d0fc5bb997728ef85
vera-rykalina <verarykalina@gmail.com>
2022-08-03 16:57:37 +0200

    Move scritps to Scripts folder

---
+++ Subtyping/Scripts/stanford_parser.py
@@ -0,0 +1,80 @@
+#!/bin/python3
+
+"""
+To play with this parser outside of pipeline
+python3 stanford_parser.py results/MS95_PRRT_20.json MS95_PRRT_20.csv
+"""
+
+
+# Import libraries
+import json, os, glob2
+import pandas as pd
+import os
+import sys
+
+
+# Open .json file
+infilename = sys.argv[1]
+outfilename = sys.argv[2]
+
+f = open(infilename, "r")
+data=json.load(f)
+f.close()
+
+name1 = infilename.rsplit("/")[-1]
+name2 = name1.split("_")[1]
+name3 = name1.rsplit(".")[-2]
+
+
+# Initiate lists and dictionary
+columns = {}
+col_header = []
+col_subtype = []
+
+# Extract useful info
+for sequence in data:
+    col_header.append(sequence["inputSequence"]["header"])
+    col_subtype.append(sequence["subtypeText"])
+
+# Add info to the dictionary
+columns["SequenceName"] = col_header
+columns["Subtype%"] = col_subtype
+
+# Create a dataframe
+df = pd.DataFrame(columns)
+
+
+# Iterate over row in df (some sequence may have NAs as subtype)
+# Create new columns "Subtype" and "Comment"
+
+for i, row in df.iterrows():
+    if row["Subtype%"] == "NA":
+        df.at[i, ["Stanford_" + name2 + "_Subtype", "Stanford_" + name2 + "_Comment"]] = "NA"
+
+    else:
+        # split subtype and % components in the string
+        # str.rstrip - splits a string by a separator, starting from the right
+        df[["Stanford_" + name2 + "_Subtype", "Stanford_" + name2 + "_Comment"]] = df["Subtype%"].str.rsplit(" ", n=1, expand=True)
+
+
+df["Stanford_" + name2 + "_Comment"] = df["Stanford_" + name2 + "_Comment"].fillna("NA")
+
+
+
+# Remove original subtype column as formatted: A (5.08%), it works in place
+#df.drop(columns=["Subtype%"], inplace = True)
+df.drop("Subtype%", axis=1, inplace = True)
+
+# Check output
+print(df.head())
+print(df.tail())
+
+# Sort df by SequenceName
+df = df.sort_values(by=["SequenceName"])
+
+# Convert a pandas dataframe to a .csv file
+df.to_csv("stanford_" + name3 + ".csv", index=False, sep=",", encoding="utf-8")
+
+
+# Remove .json file
+os.remove(infilename)
---
+++ Subtyping/Scripts/tag_parser.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+# Import libraries
+import pandas as pd
+import sys
+import re
+
+infilename = sys.argv[1]
+#outfilename = sys.argv[2]
+
+
+# Read .csv file
+f = open(infilename, "rb")
+df = pd.read_excel(f)
+f.close()
+
+
+name1 = infilename.rsplit("/")[-1] # gives a file name.csv
+name2 = name1.split("_")[-3] # gives a middle part after splitting by "_"
+name3 = name1.split(".")[-2] # cuts .xlsx
+
+
+# Select only what is needed
+df = df.loc[:,["Scount", "Header", "Sequenz"]]
+
+print(df)
+
+df["SequenceNumber"] = df["Header"].str.extract("(\d\d-\d{5,6}_\w{3,4}_\d{2})_?\w{0,}?$", expand=True)
+print(df)
+df["SeqInfo"] = df["Header"].str.extract("\d\d-\d{5,6}_\w{3,4}_\d{2}(_\w{0,}?)$")
+print(df)
+#df["SeqInfo"] = df["SeqInfo"].fillna(df["SequenceNumber"])
+#print(df)
