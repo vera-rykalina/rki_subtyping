53893b802328ce4b2d3e937b267c6eefd5206ded
vera-rykalina <verarykalina@gmail.com>
2022-08-04 23:11:38 +0200

    Add decision.py script

---
+++ Subtyping/Scripts/decision.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+# Import libraries
+import pandas as pd
+import sys
+import re
+
+# Read .csv file
+df_prrt = pd.read_csv("PRRT_joint.csv", sep = ",")
+df_env = pd.read_csv("ENV_joint.csv", sep = ",")
+df_int = pd.read_csv("INT_joint.csv", sep = ",")
+
+
+for i, row in df_prrt.iterrows():
+    if row["Stanford_PRRT_Subtype"] == row["Comet_PRRT_Subtype"] :
+        df_prrt.at[i, ["PRRT_Subtype"]] = row["Stanford_PRRT_Subtype"]
+    elif row["Comet_PRRT_Subtype"] == "_Seq. nicht klassifizierbar":
+        df_prrt.at[i, ["PRRT_Subtype"]] = "_Seq. nicht klassifizierbar"
+    else:
+        df_prrt.at[i, ["PRRT_Subtype"]] = "Manual"
+
+for i, row in df_env.iterrows():
+    if row["Stanford_ENV_Subtype"] == row["Comet_ENV_Subtype"] :
+        df_env.at[i, ["ENV_Subtype"]] = row["Stanford_ENV_Subtype"]
+    elif row["Comet_ENV_Subtype"] == "_Seq. nicht klassifizierbar":
+        df_env.at[i, ["ENV_Subtype"]] = "_Seq. nicht klassifizierbar"
+    else:
+        df_env.at[i, ["ENV_Subtype"]] = "Manual"
+    
+
+for i, row in df_int.iterrows():
+    if row["Stanford_INT_Subtype"] == row["Comet_INT_Subtype"] :
+        df_int.at[i, ["INT_Subtype"]] = row["Stanford_INT_Subtype"]
+    elif row["Comet_INT_Subtype"] == "_Seq. nicht klassifizierbar":
+        df_int.at[i, ["INT_Subtype"]] = "_Seq. nicht klassifizierbar"
+    else:
+        df_int.at[i, ["INT_Subtype"]] = "Manual"
+
+print(df_prrt[["SequenceName", "Stanford_PRRT_Subtype", "Comet_PRRT_Subtype", "PRRT_Subtype"]].head(50))
+
+print(df_env[["SequenceName", "Stanford_ENV_Subtype", "Comet_ENV_Subtype", "ENV_Subtype"]].head(50))
+
+print(df_int[["SequenceName", "Stanford_INT_Subtype", "Comet_INT_Subtype", "INT_Subtype"]].head(50))
