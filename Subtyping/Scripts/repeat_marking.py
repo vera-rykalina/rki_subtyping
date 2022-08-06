666864da1e327e19e537c13bfcc65295b52fb407
vera-rykalina <verarykalina@gmail.com>
2022-08-06 20:37:16 +0200

    Create output .fasta with marked repeats

--- Subtyping/Scripts/repeat_marking.py
+++ Subtyping/Scripts/repeat_marking.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 
 # Import libraries
-import pandas as pd
 import sys
-import re
 from Bio import SeqIO
+from collections import Counter
+
 
 infilename = sys.argv[1]
 outfilename = sys.argv[2]
@@ -12,31 +12,29 @@ outfilename = sys.argv[2]
 # Read fasta
 names = []
 sequences = []
-
 fasta_sequences = SeqIO.parse(open(infilename),'fasta')
 for fasta in fasta_sequences:
     names.append(fasta.id)
     sequences.append(str(fasta.seq))
-print(names)
-print(sequences)
-print(len(sequences))     
 
-from collections import Counter
+# Mark repeats
 counts = Counter(names)
 for name, num in counts.items():
-    print(name, num)
     if num > 1:
-        for suffix in range(1, num + 1):
-            if num == 1:
-                names[names.index(name)] = name
-            else: 
-                names[names.index(name)] = name + 'R' 
+        for suffix in range(1, num+1):
+            names[names.index(name)] = name + "repeat" + str(suffix)
 
-print(names)
-# name1 = infilename.rsplit("/")[-1] # gives a file name.fasta
-# name2 = name1.split("_")[-2] # gives a middle part after splitting by "_"
-# name3 = name1.split(".")[-2] # cuts .fasta
+# Create a new fasta file with marked samples
+marked_fasta = ""
+for i in range(len(names)):
+    marked_fasta += f"{names[i]}\n{sequences[i]}\n"
+
+print(marked_fasta)
+name1 = infilename.rsplit("/")[-1] # gives a file name.fasta
+name2 = name1.split("_")[-2] # gives a middle part after splitting by "_"
+name3 = name1.split(".")[-2] # cuts .fasta
 
 
-# with open(name3 + "R" + ".fasta", "w") as file:
-#     file.writelines()
+
+with open("../test/"+ name3 + ".fasta", "w") as file:
+    file.writelines(marked_fasta)
