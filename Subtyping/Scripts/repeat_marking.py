ba47640cc81fdfa888d0c4e36e02a580f274d278
vera-rykalina <verarykalina@gmail.com>
2022-08-08 17:48:51 +0200

    Rewrite a block to procude output fasta using wrap

--- Subtyping/Scripts/repeat_marking.py
+++ Subtyping/Scripts/repeat_marking.py
@@ -4,7 +4,7 @@
 import sys
 from Bio import SeqIO
 from collections import Counter
-
+from textwrap import wrap
 
 infilename = sys.argv[1]
 outfilename = sys.argv[2]
@@ -24,17 +24,19 @@ for name, num in counts.items():
         for suffix in range(1, num+1):
             names[names.index(name)] = name + "repeat" + str(suffix)
 
-# Create a new fasta file with marked samples
-marked_fasta = ""
-for i in range(len(names)):
-    marked_fasta += f"{names[i]}\n{sequences[i]}\n"
 
-print(marked_fasta)
+
 name1 = infilename.rsplit("/")[-1] # gives a file name.fasta
 name2 = name1.split("_")[-2] # gives a middle part after splitting by "_"
 name3 = name1.split(".")[-2] # cuts .fasta
 
 
+# Write into a file after marking repeated samples
+with open(name3 + "M" + ".fasta", "w") as file:
+    for i in range(len(names)):
+        file.writelines(">" + names[i])
+        file.writelines("\n")
+        file.writelines("\n".join(wrap(sequences[i], 60)))
+        file.writelines("\n")
 
-with open(name3 + ".fasta", "w") as file:
-    file.writelines(marked_fasta)
+
