53b4b66bc56ce8edd2547e0bb5943ec3b2653428
vera-rykalina <verarykalina@gmail.com>
2022-08-08 22:07:13 +0200

    Add a script for final_report

---
+++ Subtyping/Scripts/final_report.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+# Import libraries
+import pandas as pd
+import sys
+import re
+import functools as ft
+
+
+# Read .csv file
+df_prrt = pd.read_csv("with_decision_PRRT_joint.csv", sep = ",")
+df_env = pd.read_csv("with_decision_ENV_joint.csv", sep = ",")
+df_int = pd.read_csv("with_decision_INT_joint.csv", sep = ",")
+df_tag_prrt = pd.read_csv("tagged_MS95_Seqs_PRRT_CO20_V5.csv", sep = ",")
+df_tag_env = pd.read_csv("tagged_MS95_Seqs_ENV_CO20_V5.csv", sep = ",")
+df_tag_int = pd.read_csv("tagged_MS95_Seqs_INT_CO20_V5.csv", sep = ",")
+
+
+# Join tables
+dfs = [df_tag_prrt, df_tag_env, df_tag_int]
+df_final = ft.reduce(lambda left, right: pd.merge(left, right, on='Scount'), dfs)
+
+
+# Select only what is needed
+df_final = df_final.loc[:,["Scount", "PRRT_Subtype","INT_Subtype", "ENV_Subtype"]]
+
+df_final["PRRT_Subtype"] = df_final["PRRT_Subtype"].fillna(df_prrt["PRRT_Subtype"])
+df_final["INT_Subtype"] = df_final["INT_Subtype"].fillna(df_int["INT_Subtype"])
+df_final["ENV_Subtype"] = df_final["ENV_Subtype"].fillna(df_env["ENV_Subtype"])
+
+print(df_final)
+
+# Prepare a clean .csv file
+df_final.to_csv("final_report.csv", sep=",", index=False, encoding="utf-8")
