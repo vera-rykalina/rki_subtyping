030ed58121106cd3709a71f50e74947de2109681
vera-rykalina <verarykalina@gmail.com>
2022-08-11 23:13:55 +0200

    Rename a column - remove 'Decision' tag

--- Subtyping/Scripts/decision.py
+++ Subtyping/Scripts/decision.py
@@ -32,34 +32,34 @@ df_int.insert(0, 'Scount', col3)
 
 for i, row in df_prrt.iterrows():
     if row["Stanford_PRRT_Subtype"] == row["Comet_PRRT_Subtype"] :
-        df_prrt.at[i, ["PRRT_Subtype_Decision"]] = row["Stanford_PRRT_Subtype"]
+        df_prrt.at[i, ["PRRT_Subtype"]] = row["Stanford_PRRT_Subtype"]
     elif row["Comet_PRRT_Subtype"] == "_Seq. nicht klassifizierbar":
-        df_prrt.at[i, ["PRRT_Subtype_Decision"]] = "_Seq. nicht klassifizierbar"
+        df_prrt.at[i, ["PRRT_Subtype"]] = "_Seq. nicht klassifizierbar"
     else:
-        df_prrt.at[i, ["PRRT_Subtype_Decision"]] = "Manual"
+        df_prrt.at[i, ["PRRT_Subtype"]] = "Manual"
 
 for i, row in df_env.iterrows():
     if row["Rega_ENV_Subtype"][0] == row["Comet_ENV_Subtype"] and len(row["Rega_ENV_Subtype"]) < 3 :
-        df_env.at[i, ["ENV_Subtype_Decision"]] = row["Comet_ENV_Subtype"]
+        df_env.at[i, ["ENV_Subtype"]] = row["Comet_ENV_Subtype"]
     elif row["Comet_ENV_Subtype"] == "_Seq. nicht klassifizierbar":
-        df_env.at[i, ["ENV_Subtype_Decision"]] = "_Seq. nicht klassifizierbar"
+        df_env.at[i, ["ENV_Subtype"]] = "_Seq. nicht klassifizierbar"
     else:
-        df_env.at[i, ["ENV_Subtype_Decision"]] = "Manual"
+        df_env.at[i, ["ENV_Subtype"]] = "Manual"
     
 
 for i, row in df_int.iterrows():
     if row["Stanford_INT_Subtype"] == row["Comet_INT_Subtype"] :
-        df_int.at[i, ["INT_Subtype_Decision"]] = row["Stanford_INT_Subtype"]
+        df_int.at[i, ["INT_Subtype"]] = row["Stanford_INT_Subtype"]
     elif row["Comet_INT_Subtype"] == "_Seq. nicht klassifizierbar":
-        df_int.at[i, ["INT_Subtype_Decision"]] = "_Seq. nicht klassifizierbar"
+        df_int.at[i, ["INT_Subtype"]] = "_Seq. nicht klassifizierbar"
     else:
-        df_int.at[i, ["INT_Subtype_Decision"]] = "Manual"
+        df_int.at[i, ["INT_Subtype"]] = "Manual"
 
-print(df_prrt[["SequenceName","Rega_PRRT_Subtype", "Stanford_PRRT_Subtype", "Comet_PRRT_Subtype", "PRRT_Subtype_Decision"]].head(50))
+print(df_prrt[["SequenceName","Rega_PRRT_Subtype", "Stanford_PRRT_Subtype", "Comet_PRRT_Subtype", "PRRT_Subtype"]].head(50))
 
-print(df_env[["SequenceName","Rega_ENV_Subtype", "Stanford_ENV_Subtype", "Comet_ENV_Subtype", "ENV_Subtype_Decision"]].head(50))
+print(df_env[["SequenceName","Rega_ENV_Subtype", "Stanford_ENV_Subtype", "Comet_ENV_Subtype", "ENV_Subtype"]].head(50))
 
-print(df_int[["SequenceName","Rega_INT_Subtype", "Stanford_INT_Subtype", "Comet_INT_Subtype", "INT_Subtype_Decision"]].head(50))
+print(df_int[["SequenceName","Rega_INT_Subtype", "Stanford_INT_Subtype", "Comet_INT_Subtype", "INT_Subtype"]].head(50))
 
 # Prepare a clean .csv file
 df_prrt.to_csv("with_decision_PRRT_joint.csv", sep=",", index=False, encoding="utf-8")
