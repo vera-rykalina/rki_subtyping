38231521d40aacd9516ce019356385796b429762
vera-rykalina <verarykalina@gmail.com>
2022-08-14 23:37:36 +0200

    Add .py file for final report

---
+++ Subtyping/Scripts/report.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+# Import libraries
+import pandas as pd
+
+
+# Read .csv file
+df_full_prrt = pd.read_csv("full_prrt.csv", sep = ",")
+df_full_env = pd.read_csv("full_env.csv", sep = ",")
+df_full_int = pd.read_csv("full_int.csv", sep = ",")
+
+# Prepare final report
+final_report = df_full_prrt[["Scount", "PRRT_Subtype"]]
+
+# Rename two first columns 
+final_report.rename(columns = {"Scount":"SCount", "PRRT_Subtype": "Subtyp_PRRT"}, inplace = True)
+
+# Add other columns
+final_report["Subtyp_INT"] = df_full_int["INT_Subtype"]
+final_report["Subtyp_ENV"] = df_full_int["ENV_Subtype"]
+final_report["Subtyp_Summe"] = None
+final_report["Env_FPR"] = ""
+
+final_report.to_excel("_subtype_uploads.xlsx", index=False, encoding="utf-8")
