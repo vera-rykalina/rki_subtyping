774e2ff13a079fd4e2ba624ccee17178b5e947eb
vera-rykalina <verarykalina@gmail.com>
2022-08-15 01:29:17 +0200

    Add .copy() for to build columns

--- Subtyping/Scripts/report.py
+++ Subtyping/Scripts/report.py
@@ -10,15 +10,15 @@ df_full_env = pd.read_csv("full_env.csv", sep = ",")
 df_full_int = pd.read_csv("full_int.csv", sep = ",")
 
 # Prepare final report
-final_report = df_full_prrt[["Scount", "PRRT_Subtype"]]
+final_report = df_full_prrt[["Scount", "PRRT_Subtype"]].copy()
 
 # Rename two first columns 
 final_report.rename(columns = {"Scount":"SCount", "PRRT_Subtype": "Subtyp_PRRT"}, inplace = True)
 
 # Add other columns
-final_report["Subtyp_INT"] = df_full_int["INT_Subtype"]
-final_report["Subtyp_ENV"] = df_full_env["ENV_Subtype"]
+final_report["Subtyp_INT"] = df_full_int["INT_Subtype"].copy()
+final_report["Subtyp_ENV"] = df_full_env["ENV_Subtype"].copy()
 final_report["Subtyp_Summe"] = None
-final_report["Env_FPR"] = ""
+final_report["Env_FPR"] = None
 
 final_report.to_excel("_subtype_uploads.xlsx", index=False, encoding="utf-8")
