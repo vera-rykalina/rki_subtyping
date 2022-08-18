841df7c6738ebfa4a20ad3591fda676fc07ef34a
vera-rykalina <verarykalina@gmail.com>
2022-08-19 00:15:29 +0200

    Replace adding columns by copying with double merge

--- Subtyping/Scripts/report.py
+++ Subtyping/Scripts/report.py
@@ -9,15 +9,29 @@ df_full_prrt = pd.read_excel("full_PRRT.xlsx")
 df_full_env = pd.read_excel("full_ENV.xlsx")
 df_full_int = pd.read_excel("full_INT.xlsx")
 
-# Prepare final report
-final_report = df_full_prrt[["Scount", "PRRT_Subtype"]].copy()
+
+# Select only what is needed
+df_full_prrt = df_full_prrt.loc[:,["Scount", "PRRT_Subtype"]]
+df_full_int = df_full_int.loc[:,["Scount", "INT_Subtype"]]
+df_full_env = df_full_env.loc[:,["Scount", "ENV_Subtype"]]
+
+
+# Merge tables
+final_report = df_full_prrt.merge(df_full_int, on = "Scount", how = "outer").merge(df_full_env, on = "Scount", how = "outer")
 
 # Rename two first columns 
-final_report.rename(columns = {"Scount":"SCount", "PRRT_Subtype": "Subtyp_PRRT"}, inplace = True)
+final_report.rename(columns = {"Scount":"SCount", "PRRT_Subtype": "Subtyp_PRRT","INT_Subtype": "Subtyp_INT", "ENV_Subtype": "Subtyp_ENV"}, inplace = True)
 
-# Add other columns
-final_report["Subtyp_INT"] = df_full_int["INT_Subtype"].copy()
-final_report["Subtyp_ENV"] = df_full_env["ENV_Subtype"].copy()
+# #  Prepare final report
+# final_report = df_full_prrt[["Scount", "PRRT_Subtype"]].copy()
+
+# # Rename two first columns 
+# final_report.rename(columns = {"Scount":"SCount", "PRRT_Subtype": "Subtyp_PRRT"}, inplace = True)
+
+# # Add other columns
+# final_report["Subtyp_INT"] = df_full_int["INT_Subtype"].copy()
+# final_report["Subtyp_ENV"] = df_full_env["ENV_Subtype"].copy()
+
 final_report["Subtyp_Summe"] = None
 final_report["Env_FPR"] = None
 
