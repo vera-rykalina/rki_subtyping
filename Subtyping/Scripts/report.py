bdf3b064ed490de7a189793add27288056c4aba8
vera-rykalina <verarykalina@gmail.com>
2022-08-18 14:03:28 +0200

    Add a code block for decision in Subtyp_Summe column

--- Subtyping/Scripts/report.py
+++ Subtyping/Scripts/report.py
@@ -21,4 +21,14 @@ final_report["Subtyp_ENV"] = df_full_env["ENV_Subtype"].copy()
 final_report["Subtyp_Summe"] = None
 final_report["Env_FPR"] = None
 
+# Make a decision
+for i, row in final_report.iterrows():
+    if row["Subtyp_PRRT"] == row["Subtyp_INT"] and row["Subtyp_PRRT"] == row["Subtyp_ENV"]:
+        final_report.at[i, ["Subtyp_Summe"]] = row["Subtyp_PRRT"]
+    elif row["Subtyp_PRRT"] == "_Seq. nicht klassifizierbar" or row["Subtyp_INT"] == "_Seq. nicht klassifizierbar":
+        final_report.at[i, ["Subtyp_Summe"]] = "_Seq. nicht klassifizierbar"
+    else:
+        final_report.at[i, ["Subtyp_Summe"]] = "Manual"
+
+
 final_report.to_excel("_subtype_uploads.xlsx", index=False, encoding="utf-8")
